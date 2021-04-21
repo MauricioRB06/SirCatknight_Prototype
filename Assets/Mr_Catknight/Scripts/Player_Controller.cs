77d3b2732f3c47abba6b785c09476c9dfb0b5fa5
MauricioRB06 <mrodriguezbec@unal.edu.co>
2021-04-21 13:50:55 -0500

    Prototype Levels
    Prototipado de niveles inicial

--- Assets/Mr_Catknight/Scripts/Player_Controller.cs
+++ Assets/Mr_Catknight/Scripts/Player_Controller.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/* Documentación:
+ 
+ https://docs.unity3d.com/es/530/Manual/class-FrictionJoint2D.html
+ https://docs.unity3d.com/ScriptReference/Collision2D.GetContacts.html
+
+ */
+
 public class Player_Controller : MonoBehaviour
 {
     public float Speed;              // Para regular la velocidad
     public float JumpForce;          // Para regular la fuerza de salto
 
     private Rigidbody2D Rigidbody2D; // Para guardar referencia al Rigidbody del jugador
+    private Animator Animator;
     private float horizontal;        // Controlar el movimiento horizontal
     private bool grounded;           // Para controlar si estamos en el suelo
 
@@ -16,15 +25,21 @@ public class Player_Controller : MonoBehaviour
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
+
 
     // Se llama en cada frame del juego
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        if (horizontal < 0.0f) transform.localScale = new Vector3(-0.75f, 0.75f, 0.75f);
+        else if(horizontal > 0.0f) transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
 
-        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);  // Test Raycast
-        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        Animator.SetBool("walk", horizontal != 0.0f);
+
+        Debug.DrawRay(transform.position, Vector3.down * 1f, Color.red);  // Test Raycast
+        if (Physics2D.Raycast(transform.position, Vector3.down, 1f))
         {
             grounded = true;
         }
