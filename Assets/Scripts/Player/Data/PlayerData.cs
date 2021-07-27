100f22aad803d59d4384fda04f429c349002f947
Mauricio Rodriguez Becerra <mrodriguezbec@unal.edu.co>
2021-07-27 16:58:04 -0500

    Git LFS Update
    add .wav Files to Git LFS

--- Assets/Scripts/Player/Data/PlayerData.cs
+++ Assets/Scripts/Player/Data/PlayerData.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
-using UnityEngine.Serialization;
 
-/* Documentation:
+// The purpose of this Script is:
+// Insert Here.
+
+/* Documentation and References:
  * 
  * Attributes: https://docs.unity3d.com/Manual/Attributes.html
  * ScriptableObject: https://docs.unity3d.com/Manual/class-ScriptableObject.html
@@ -23,8 +25,7 @@ namespace Player.Data
 
         [Header("Jump State")]
         public float jumpForce = 15f;
-        [Range(0f, 2f)]
-        public int amountOfJumps = 1;
+        [Range(0f, 2f)] public int amountOfJumps = 1;
 
         [Header(("Dodge Roll"))]
         public float dodgeRollImpulse = 30f;
@@ -32,13 +33,14 @@ namespace Player.Data
 
         [Header("Wall Jump State")]
         public float wallJumpVelocity = 20;
-        // We use it to avoid that after jumping the player can return to the wall immediately after
-        public float wallJumpTime = 0.4f;
         public Vector2 wallJumpAngle = new Vector2(1, 2);
-
+        
+        // We use it to avoid that after jumping the player can return to the wall immediately after.
+        public float wallJumpTime = 0.4f;
+        
         [Header("In Air State")]  
         public float coyoteTime = 0.2f;
-        // We use it to limit the jump the moment the player releases the button before completing a full jump 
+        // We use it to limit the jump the moment the player releases the button before completing a full jump.
         public float jumpHeightLimiter = 0.5f;
 
         [Header("Wall Slide State")]  
@@ -53,22 +55,31 @@ namespace Player.Data
 
         [Header("Dash State")] 
         public float dashCooldown = 5f;
+        
         /* We use it to modify the time scale of the game and make the effect of slow motion, as well as to know
          the maximum time that we will be in that weaponState */
         public float dashMaxHoldTime = 1f;
         public float dashHoldTimeScale = 0.25f;
-        // It is the life time of the Dash movement, once the timeout is reached or the player releases the button
+        
+        // It is the life time of the Dash movement, once the timeout is reached or the player releases the button.
         public float dashLifeTime = 0.2f;
-        // The speed that we are going to apply to the player
+        
+        // The speed that we are going to apply to the player.
         public float dashImpulseVelocity = 30f;
-        // We use it to limit the distance traveled by the Dash on the Y axis
+        
+        // We use it to limit the distance traveled by the Dash on the Y axis.
         public float dashEndYLimiter = 0.2f;
-        // We use it to separate the images from the AfterImagePool
+        
+        // We use it to separate the images from the AfterImagePool.
         public float distanceBetweenAfterImages = 0.5f;
         
         [Header("Crouch States")]
         public float crouchMovementVelocity = 3f;
         public float crouchColliderHeight = 1.3f;
         public float normalColliderHeight = 1.6f;
+
+        [Header("Attack State")]
+        public float decreaseGravityScale = 0.75f;
+        public float restoreGravityScale = 3.0f;
     }
 }
