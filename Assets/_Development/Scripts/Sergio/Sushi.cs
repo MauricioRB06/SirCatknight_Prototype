54f03e4ae2a367fe0d302de23cb58e87b550c0b9
LuisVaca1503 <lvacar@unal.edu.co>
2021-08-24 00:43:46 -0500

    Added Palette Level 2
    Added Palette for level 2, and created a new damageable object.

---
+++ Assets/_Development/Scripts/Sergio/Sushi.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interfaces;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+
+public class Sushi : MonoBehaviour, IDamageableObject
+
+{
+    [SerializeField] private bool damaged = false;
+    [SerializeField] private GameObject sushiParticle;
+
+    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            TakeDamage(0);
+        }
+    }
+    public void TakeDamage(float amount)
+    {
+        Instantiate(sushiParticle, transform.position, Quaternion.identity);
+        Destroy(gameObject, 0f);
+    }
+    
+}
